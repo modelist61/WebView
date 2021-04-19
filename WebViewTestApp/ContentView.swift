77ce58d169cad6a99c9d285f210382b044207083
Dmitry Tokarev <modelist61@bk.ru>
2021-04-19 18:43:04 +0300

    Wirth tabView

--- WebViewTestApp/ContentView.swift
+++ WebViewTestApp/ContentView.swift
@@ -11,33 +11,54 @@ import WKView
 struct ContentView: View {
     
     @State private var isSheetPresented = false
+    let asf = WKView.ActivityIndicator.self
+    
     
     var body: some View {
-        NavigationView {
-            VStack {
-                Divider()
-                
-                NavigationLink("ВСЕ КУПОНЫ", destination: WebView(url: "https://www.vsemitut.ru"))
-                
-                Button(action: {
-                    isSheetPresented.toggle()
-                }, label: {
-                    Text("Купоны в H2O")
-                }).sheet(isPresented: $isSheetPresented, content: {
-                    NavigationView {
-                        WebView(url: "https://www.vsemitut.ru/h2opark/",
-                                tintColor: .red,
-                                titleColor: .yellow,
-                                backText: Text("Cancel"),
-                                reloadImage: Image(systemName: "figure.wave"),
-                                goForwardImage: Image(systemName: "arrowshape.turn.up.right.circle"),
-                                goBackImage: Image(systemName: "arrowshape.turn.up.left.circle"))
-                    }
-                })
-                Spacer()
+        
+        TabView {
+            NavigationView {
+                WebView(url: "https://www.vsemitut.ru/h2opark/",
+                        tintColor: .black,
+                        titleColor: .red,
+                        hidesBackButton: true,
+                        reloadImage: Image(systemName: "")
+                        
+                        //                            goForwardImage: <#T##Image#>,
+                        //                            goBackImage: <#T##Image#>,
+                        //                            title: "Загружаемся..."
+                        //                            allowedHosts: <#T##[String]?#>,
+                        //                            forbiddenHosts: <#T##[String]?#>,
+                        //                            credential: <#T##URLCredential?#>,
+                        //
+                )
             }
-            .navigationBarTitle("ВсеМыТут")
-            .navigationBarTitleDisplayMode(.inline)
+            .tabItem {
+                Text("Главная")
+                Image(systemName: "house.fill")
+                
+            }
+            NavigationView {
+                
+                WebView(url: "https://www.vsemitut.ru/greenwich/")
+                
+            }
+            .tabItem {
+                Text("Second")
+                Image(systemName: "phone.fill")
+            }
+            WebView(url: "https://www.vsemitut.ru/personal/")
+                .tabItem {
+                    Text("Личный кабинет")
+                    Image(systemName: "phone.fill")
+                }
+            
+            Text("Test")
+                
+                .tabItem {
+                    Text("TEST")
+                    Image(systemName: "phone.fill")
+                }
         }
     }
 }
@@ -47,3 +68,24 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+
+struct TabBarIcon: View {
+     
+     let width, height: CGFloat
+     let systemIconName, tabName: String
+     
+     
+     var body: some View {
+         VStack {
+             Image(systemName: systemIconName)
+                 .resizable()
+                 .aspectRatio(contentMode: .fit)
+                 .frame(width: width, height: height)
+                 .padding(.top, 10)
+             Text(tabName)
+                 .font(.footnote)
+             Spacer()
+         }
+     }
+ }
